258b8a14c2a74626f60f777f93911b6e123a4303
Alexander Liu <a@alexanderliu.com>
2025-10-04 22:15:23 -0700

    feat: home page images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	type Image = {
+		default: {
+			sources: {
+				avif: string;
+				webp: string;
+			};
+			img: {
+				src: string;
+				w: number;
+				h: number;
+			};
+		};
+	};
+
+	export const imageModules = import.meta.glob<Image>('$lib/assets/photos/*.avif', {
+		eager: true,
+		query: { enhanced: true, format: 'avif;webp;jpeg;', w: '' }
+	});
+
+	onMount(() => {
+		Promise.all(
+			Array.from(document.querySelectorAll<HTMLImageElement>('.home-image'))
+				.filter((img) => !img.complete)
+				.map(
+					(img) =>
+						new Promise((resolve) => {
+							img.onload = img.onerror = resolve;
+						})
+				)
+		).then(() => {
+			console.log('images finished loading');
+		});
+	});
+</script>
+
+{#each Object.entries(imageModules) as [_path, module]}
+	<enhanced:img class="image home-image" src={module.default} alt="" />
+{/each}
+
+<style>
+	.image {
+		width: 75vw;
+		height: 75vh;
+		object-fit: contain;
+	}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
+	plugins: [enhancedImages(), sveltekit(), devtoolsJson()]
 });
