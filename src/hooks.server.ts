822a3ade97999b7f14025a340708342b6f7465ff
Alexander Liu <a@alexanderliu.com>
2025-10-10 22:43:03 -0700

    fix: optional dev rewrite url

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,14 @@
 import { dev } from '$app/environment';
-import { DEV_AUTH_REDIRECT_URL } from '$env/static/private';
+import * as env from '$env/static/private';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (dev && event.url.pathname.startsWith('/auth')) {
+	if (dev && env.DEV_AUTH_REDIRECT_URL && event.url.pathname.startsWith('/auth')) {
 		const originalUrl = new URL(event.request.url);
-		const newUrl = new URL(`${originalUrl.pathname}${originalUrl.search}`, DEV_AUTH_REDIRECT_URL);
+		const newUrl = new URL(
+			`${originalUrl.pathname}${originalUrl.search}`,
+			env.DEV_AUTH_REDIRECT_URL
+		);
 
 		return await fetch(new Request(newUrl, event.request), { redirect: 'manual' });
 	}
