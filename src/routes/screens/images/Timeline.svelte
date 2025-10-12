4f2ff2debd765790bcdf7cffc4a85b7fd8aac59d
Alexander Liu <a@alexanderliu.com>
2025-10-12 15:50:17 -0700

    feat: timeline bottom

--- src/routes/screens/images/Timeline.svelte
+++ src/routes/screens/images/Timeline.svelte
@@ -14,26 +14,37 @@
 	);
 </script>
 
-<div class="timeline">
-	{#each datetimePercentages as datetimePercentage, i}
-		<div
-			class="marker"
-			class:active={i + 1 == activePhoto}
-			style:left="{datetimePercentage}%"
-		></div>
-	{/each}
+<div class="parent">
+	<div class="timeline">
+		{#each datetimePercentages as datetimePercentage, i}
+			<div
+				class="marker"
+				class:active={i + 1 == activePhoto}
+				style:left="{datetimePercentage}%"
+			></div>
+		{/each}
+	</div>
 </div>
 
 <style>
+	.parent {
+		position: fixed;
+		margin: 0 16px;
+		left: 0;
+		right: 0;
+		bottom: 0;
+	}
+
 	.timeline {
-		/* font-family: 'Commit Mono', monospace; */
 		position: relative;
-		max-width: 60%;
-		width: 100%;
+		margin: 0 auto;
+		height: 24px;
+		max-width: 768px;
 	}
 
 	.marker {
 		position: absolute;
+		margin-top: 8px;
 		height: 16px;
 		width: 1px;
 		background-color: var(--sand-9);
@@ -41,6 +52,7 @@
 
 	.active {
 		background-color: var(--blue-9);
+		margin-top: 0;
 		z-index: 10;
 		height: 24px;
 	}
