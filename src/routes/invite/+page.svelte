24432a5e63a4e7fa36219a7e7dd1d40af7ba3b4d
Alexander Liu <a@alexanderliu.com>
2025-10-12 01:44:56 -0700

    feat: invite text

---
+++ src/routes/invite/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
+<p class="text">
+	Hello! You are cordially invited to Alexander's 22nd birthday celebration on Oct 18th.
+</p>
+
+<style>
+	.text {
+		font-family: 'Archivo Variable', sans-serif;
+	}
+</style>
