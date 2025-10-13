345f9c2633eb13f9f5d4466719e6c167db7d9684
Alexander Liu <a@alexanderliu.com>
2025-10-12 22:16:15 -0700

    feat: invite page

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	interface Props {
+		text: string;
+	}
+
+	const { text }: Props = $props();
+</script>
+
+<div class="parent">
+	<button class="button">
+		{text}
+	</button>
+</div>
+
+<style>
+	.parent {
+		padding: 1px;
+		outline-color: var(--blue-9);
+		outline-offset: -1px;
+		border: 1px solid var(--sand-7);
+		display: block;
+	}
+
+	.parent:focus-within {
+		outline-width: 2px;
+	}
+
+	.button {
+		font-size: 0.875rem;
+		line-height: 1.25rem;
+		outline: none;
+		border: none;
+		border-radius: 1000px;
+		background-color: var(--blue-9);
+		height: 2.75rem;
+		width: 100%;
+	}
+
+	.button:hover {
+		background-color: var(--blue-10);
+	}
+
+	.button:active {
+		filter: brightness(108%);
+	}
+</style>
--- src/routes/screens/invite/Invite.svelte
+++ src/routes/screens/invite/Invite.svelte
@@ -1,46 +1,80 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+
 	const messageText =
-		"Hello!\nYou are cordially invited to Alexander's 22nd birthday celebration on Oct 18th.";
+		"Hello!\nYou are cordially invited to Alexander's 22nd birthday celebration on Oct 18th.\nPlease sign in to RSVP and create your doodle.";
 
 	const message = $derived.by(() => {
-		const delaySpacing = 15;
+		const delaySpacing = 10;
 		let delayAggregator = 500;
 
-		return messageText.split('\n').map((line) => {
-			const lineObject = {
-				words: line.split(' ').map((word) => {
-					const wordObject = { word, animationDelay: delayAggregator };
-					delayAggregator += delaySpacing;
-					return wordObject;
-				})
-			};
-			delayAggregator += 500;
-			return lineObject;
-		});
+		return {
+			lines: messageText.split('\n').map((line) => {
+				const lineObject = {
+					words: line.split(' ').map((word) => {
+						const wordObject = { word, animationDelay: delayAggregator };
+						delayAggregator += delaySpacing;
+						return wordObject;
+					})
+				};
+				delayAggregator += 50;
+				return lineObject;
+			}),
+			totalDelay: delayAggregator
+		};
 	});
 </script>
 
-{#each message as line}
-	<p>
-		{#each line.words as { word, animationDelay }}
-			<span class="word" style:animation-delay="{animationDelay}ms">
-				{`${word} `}
-			</span>
-		{/each}
-	</p>
-{/each}
+<div class="center">
+	<div class="x-spacing">
+		<div class="line"></div>
+		<div class="right-line-parent">
+			<div class="line"></div>
+		</div>
+		<div class="message">
+			{#each message.lines as line}
+				<p>
+					{#each line.words as { word, animationDelay }}
+						<span class="word" style:animation-delay="{animationDelay}ms">
+							{`${word} `}
+						</span>
+					{/each}
+				</p>
+			{/each}
+		</div>
+		<div class="button" style:animation-delay="{message.totalDelay + 100}ms">
+			<Button text="Sign In" />
+		</div>
+	</div>
+</div>
 
 <style>
-	@keyframes text-appear {
-		0% {
-			filter: blur(4px);
-			opacity: 0;
-		}
+	.center {
+		margin: 0 auto;
+		max-width: 35rem;
+	}
 
-		100% {
-			filter: blur(0px);
-			opacity: 1;
-		}
+	.x-spacing {
+		position: relative;
+		margin: 0 16px;
+	}
+
+	.line {
+		background-color: var(--sand-4);
+		top: 0;
+		width: 1px;
+		height: 100vh;
+		position: fixed;
+		z-index: -1;
+	}
+
+	.right-line-parent {
+		position: absolute;
+		right: 1px;
+	}
+
+	.message {
+		margin: 96px 0 48px 0;
 	}
 
 	p {
@@ -48,13 +82,41 @@
 		font-weight: 450;
 		font-size: 1.25rem;
 		font-variation-settings: 'wdth' 120;
-		max-width: 30rem;
 		line-height: 1.4;
-		margin: 16px auto;
+		margin: 16px 0;
+	}
+
+	@keyframes text-appear {
+		0% {
+			filter: blur(4px);
+			opacity: 0;
+		}
+
+		100% {
+			filter: none;
+			opacity: 1;
+		}
 	}
 
 	.word {
 		animation-name: text-appear;
+		animation-duration: 750ms;
+		animation-fill-mode: both;
+		animation-timing-function: cubic-bezier(0.33, 1, 0.68, 1);
+	}
+
+	@keyframes appear {
+		0% {
+			opacity: 0;
+		}
+
+		100% {
+			opacity: 1;
+		}
+	}
+
+	.button {
+		animation-name: appear;
 		animation-duration: 750ms;
 		animation-fill-mode: both;
 		animation-timing-function: cubic-bezier(0.33, 1, 0.68, 1);
