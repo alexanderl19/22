6e183359193da0acf62d296c9bcd25e7dea138ac
Alexander Liu <a@alexanderliu.com>
2025-10-13 22:52:10 -0700

    feat: redirect to invite page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,21 @@
 <script lang="ts">
-	// import Images from './screens/images/Images.svelte';
+	import { goto } from '$app/navigation';
+	import Images from './screens/images/Images.svelte';
 	import Loading from './screens/loading/Loading.svelte';
 
-	let stage = $state<'loading' | 'photos' | 'invite' | 'list'>('invite');
-
+	let loading = $state(true);
 	let loadingPercentage = $state(0);
 
 	const onLoadingDone = () => {
-		if (stage == 'loading') {
-			stage = 'photos';
-		}
+		loading = false;
 	};
 
 	const onAnimationDone = () => {
-		stage = 'invite';
+		goto('/invite');
 	};
-
-	$effect(() => {});
 </script>
 
-{#if stage === 'loading'}
+{#if loading}
 	<Loading {loadingPercentage} />
 {/if}
-{#if stage === 'loading' || stage === 'photos'}
-	<!-- <Images visible={stage == 'photos'} {onLoadingDone} {onAnimationDone} bind:loadingPercentage /> -->
-{/if}
+<Images visible={!loading} {onLoadingDone} {onAnimationDone} bind:loadingPercentage />
